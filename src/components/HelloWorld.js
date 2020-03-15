0cd5c5452dcfd3b5be35968c7f8054bd866d2084
Angelo Liwanag <AngeloLiwanag75@gmail.com>
2020-03-15 02:34:56 -0700

    Synthetic events with State

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 
+const Button = (props) => {
+    return (
+    <button onClick={props.handleClick}>Clicked {props.count} times</button>
+    )
+}
+
 class HelloWorld extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            clickCount: 0
+        }
+    }
+
+    handleClick = () => {
+        this.setState({
+            clickCount: this.state.clickCount + 1
+        });
+    }
+
     render() {
         return (
             <div>
                 <p>{this.props.someText}</p>
                 {this.props.children}
+                <Button handleClick={this.handleClick} count={this.state.clickCount}/>
             </div>
         );
     }

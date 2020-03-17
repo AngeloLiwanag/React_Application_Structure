b8e8b438bafd08f5ad042aaba534bb1789eddd96
Angelo Liwanag <AngeloLiwanag75@gmail.com>
2020-03-16 22:54:12 -0700

    Learning hooks

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React, { useState } from 'react';
+import styles from './MyButtonComponent.module.css'
 
 const Button = (props) => {
     return (
@@ -6,29 +8,54 @@ const Button = (props) => {
     )
 }
 
-class HelloWorld extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            clickCount: 0
-        }
-    }
+const HelloWorld = props => {
+    // const [state, setState] = useState({
+    //     clickCount: 0
+    // });
 
-    handleClick = () => {
-        this.setState({
-            clickCount: this.state.clickCount + 1
-        });
+    // const handleClick = e => {
+    //     e.preventDefault();
+    //     setState({
+    //         clickCount: state.clickCount + 1
+    //     });
+    // }
+    const [count, setCount] = useState(0);
+ 
+    const handleClick = e => {
+        e.preventDefault();
+        setCount(count + 1);
     }
+ 
+    return(
+        <div>
+            {count}
+            <button className={styles.myButton} onClick={handleClick}>Click Me</button>
+        </div>
+    )
+}   
+// class HelloWorld extends Component {
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             clickCount: 0
+//         }
+//     }
 
-    render() {
-        return (
-            <div>
-                <p>{this.props.someText}</p>
-                {this.props.children}
-                <Button handleClick={this.handleClick} count={this.state.clickCount}/>
-            </div>
-        );
-    }
-}
+//     handleClick = () => {
+//         this.setState({
+//             clickCount: this.state.clickCount + 1
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <p>{this.props.someText}</p>
+//                 {this.props.children}
+//                 <Button handleClick={this.handleClick} count={this.state.clickCount}/>
+//             </div>
+//         );
+//     }
+// }
 
 export default HelloWorld;
